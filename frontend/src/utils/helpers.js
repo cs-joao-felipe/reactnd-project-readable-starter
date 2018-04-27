a475489bdbb50fb19e8094b50be30d8751f07b4a
Joao Felipe Santoro Pinto <joao.felipe@concrete.com.br>
2018-04-26 21:47:58 -0300

    feat: Implementing guid Helper
    Creating a helper function to generate a UUID for usage later on posts/comments creation

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,3 +1,15 @@
 export const getJSONDate = () => {
-    return (new Date).toJSON()
+    return (new Date()).toJSON()
+}
+
+
+export const guid = () => {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
 }
